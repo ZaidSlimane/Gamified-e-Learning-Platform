b2b51f31b908fbb4c245bd4994ee03ce6560abea
Zaid Slimane <102682510+ZaidSlimane@users.noreply.github.com>
2024-04-26 15:08:57 +0100

    auth-update

--- ElearningPlatform/urls.py
+++ ElearningPlatform/urls.py
@@ -9,9 +9,9 @@ from .views import *
 
 urlpatterns = [
     path('hello-world/', views.hello_world, name='hello_world'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('login/', views.Login.as_view(), name='login'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('home/', views.home.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('courses/', CourseListCreateAPIView.as_view(), name='course-list'),
     path('courses/<int:pk>/', CourseRetrieveUpdateDestroyAPIView.as_view(), name='course-detail'),
